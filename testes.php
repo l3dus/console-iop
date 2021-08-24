379aa9235a3166a926eef497310db10ef9bb25c2
Eduardo Santos <34162869+l3dus@users.noreply.github.com>
2021-08-24 19:08:09 -0300

    Update testes.php

--- testes.php
+++ testes.php
@@ -16,7 +16,7 @@ use Console\Service\RecuperaTicketsResolvidos5Dias;
 //$exec = $updateStatus->execute();
 //print_r($exec);
 
-$url = 'http://cadi-dev.cloud4biz.com/cit-esi-web/rest/esi/execute/resolvido?response=retorno';
+$url = 'http://xxxxxxxxxxxxxxxxxxxxxxxxxxxx/cit-esi-web/rest/esi/execute/resolvido?response=retorno';
 
 $headers = [
     'authentication-token: eyJhbGciOiJIUzUxMiJ9.eyJleHAiOjE2MjM4ODEyNTgsIm5hbWUiOiJMdWl6IEVkdWFyZG8gU291emEgQW1vcmltIGRvcyBTYW50b3MiLCJjb250cm9sIjoiN2IyMjY5NzAyMjNhMjIzMTMwMmUzMDJlMzkyZTM2MzYyMjJjMjI2ODZmNzM3NDIyM2EyMjMxMzAyZTMwMmUzOTJlMzYzNjIyN2QiLCJpc3N1ZWRBdCI6MTYyMzg3NDA1ODI0MiwibG9jYWxlIjoiZW4iLCJjbGllbnRfaWQiOiJ1bmtub3duIiwiZXhwaXJlc0F0IjoxNjIzODgxMjU4MjQyLCJ0aW1lb3V0Ijo3MjAwLCJ1c2VybmFtZSI6IkRQT0FUSjFcXGwuZXNhbnRvcyJ9.n2NCkFXfrgVnqD9JRhHkcJIY0-ypAJNTRbGcvf7MrcxN8sDagCTzfF9Z3LSoX2HFP4pZKyuiLNee5oTHH06yiQ',
